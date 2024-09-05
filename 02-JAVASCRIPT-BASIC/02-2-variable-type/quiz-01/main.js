93942a65e698cf2ddfaaf81754c1752e2433fd30
beofme31 <beome31@naver.com>
2024-09-05 10:50:00 +0900

    변수 선언, 변경 및 퀴즈

---
+++ 02-JAVASCRIPT-BASIC/02-2-variable-type/quiz-01/main.js
@@ -0,0 +1,11 @@
+let name = '이선재';
+let introduce = '비전공자이지만 취미로 공부하다가 프론트엔드 개발자가 되기 위해 참가했습니다.';
+let wish = '실제 홈페이지를 개발하고 싶습니다 ';
+let message = '각자의 목표를 달성할 수 있도록 낙오 없이 같이 으쌰으쌰하며 힘내봅시당ㅎㅎ';
+
+document.write(
+  '<div><b>이름:</b> ' + name +  '</div>',
+  '<div><b>자기소개:</b> ' + introduce +  '</div>',
+  '<div><b>프론트엔드 스쿨 11기를 통해 이루고 싶은 점:</b> ' + wish +  '</div>',
+  '<div><b>동료들과 나에게 보내는 응원메시지:</b> ' + message +  '</div>'
+)
