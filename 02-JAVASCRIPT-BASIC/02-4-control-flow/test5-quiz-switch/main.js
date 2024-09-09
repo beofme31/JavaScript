88ba00b3a35588dd7415fb4695fcbaafc29ed534
beofme31 <beome31@naver.com>
2024-09-09 21:34:27 +0900

    test5-quiz-switch 복습

--- 02-JAVASCRIPT-BASIC/02-4-control-flow/test5-quiz-switch/main.js
+++ 02-JAVASCRIPT-BASIC/02-4-control-flow/test5-quiz-switch/main.js
@@ -8,7 +8,7 @@ const ifTest = () => {
   }else if(isNaN(month) || month <1 || month > 12){
     resultNode.innerHTML = '잘못 입력하였습니다. '
   }else {
-    // 계절 판단
+    //계절 판단
     let season
     month = parseInt(month)
     if(month === 12 || month === 1 || month === 2){
@@ -20,13 +20,13 @@ const ifTest = () => {
     }else if(month === 9 || month === 10 || month === 11){
       season = '가을'
     }
-    // 결과 화면 출력
+    //결과 화면 출력
     resultNode.innerHTML = `입력하신 월은 ${month}이며, ${season}입니다.`
   }
 }
 
 const switchTest = () => {
-  // swtch - case 문으로 작성
+  //switch - case 문으로 작성
   let resultNode = document.querySelector('#result')
   let month = prompt('월을 숫자로 입력하세요.')
   if(month == null){
@@ -34,36 +34,33 @@ const switchTest = () => {
   }else if(isNaN(month) || month <1 || month > 12){
     resultNode.innerHTML = '잘못 입력하였습니다. '
   }else {
+    //계절 판단
     let season
     month = parseInt(month)
-    switch (month) {
+
+    switch(month){
       case 12:
       case 1:
-      case 2:{
-        season = '겨울';
+      case 2:
+        season = '겨울'
         break;
-      }
       case 3:
       case 4:
-      case 5:{
-        season = '봄';
+      case 5:
+        season = '봄'
         break;
-      }
       case 6:
       case 7:
-      case 8:{
-        season = '여름';
+      case 8:
+        season = '여름'
         break;
-      }
       case 9:
       case 10:
-      case 11:{
-        season = '가을';
+      case 11:
+        season = '가을'
         break;
-      }
     }
-    
-    // 결과 화면 출력
-    resultNode.innerHTML = `입력하신 월은 ${month}월이며, ${season}입니다.`
+    //결과 화면 출력
+    resultNode.innerHTML = `입력하신 월은 ${month}이며, ${season}입니다.`
   }
 }
