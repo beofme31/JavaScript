329f51e37a752a092966808fe53d349b047e6c13
beofme31 <beome31@naver.com>
2024-09-11 11:37:50 +0900

    function

--- 02-JAVASCRIPT-BASIC/02-4-control-flow/test9-quiz-while/main.js
+++ 02-JAVASCRIPT-BASIC/02-4-control-flow/test9-quiz-while/main.js
@@ -1,14 +1,12 @@
-let i = 1;
-while (i <= 9) {
-  document.write("<div>");
-  document.write(`<h3>${i}단</h3>`);
-
-  let j = 1;
-  while (j <= 9) {
-    document.write(`${i}X${j}=${i*j} <br />`);
-    j++;
-  }
-
-  document.write("</div>");
-  i++;
+let i = 1
+while(i < 10){
+  document.write("<div>")
+  document.write(`<h3>${i}단</h3>`)
+    let j = 1
+    while(j < 10){
+      document.write(`${i} X ${j} = ${i * j} <br />`)
+      j++
+    }
+  document.write("</div>")
+  i++
 }
