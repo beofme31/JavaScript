c3d202f1b6ed9cf24154323398d70ee52ea2b298
beofme31 <beome31@naver.com>
2024-09-12 14:48:41 +0900

    이벤트 form

---
+++ 02-JAVASCRIPT-BASIC/02-7-event/test4-event-form/main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let result = document.getElementById('result')
+let form = document.getElementById('form')
+
+function printResult(msg){
+  result.innerHTML = msg 
+}
+
+form.addEventListener('submit', () => {
+  // form의 submit은 브라우저에 기본으로 서버 요청, 결과로 화면 갱신처리
+  printResult('submit event')
+})
+form.addEventListener('reset', () => {
+  printResult('reset event')
+})
+
+let input1 = document.getElementById('input1')
+let select1 = document.getElementById('select1')
+
+input1.addEventListener('focus', () => {
+  // 색상 변경
+  // 현재 이벤트가 발생한 DOM node 객체 획득
+  // event 객체를 통해서 현재 이벤트가 발생한 node 객체 획득 가능
+
+  e.target.style.background = 'pink'
+})
+input1.addEventListener('blur', (e) => {
+  e.target.style.background = ''
+  // 유저 입력 데이터 획득
+  printResult(`input data: ${e.target.value}`)
+})
+
+select1.addEventListener('focus', (e) => {
+  e.target.style.background = 'pink'
+})
+select1.addEventListener('change', (e) => {
+  printResult(`input change: ${e.target.value}`)
+})
+select1.addEventListener('blur', (e) => {
+  e.target.style.background = ''
+})
