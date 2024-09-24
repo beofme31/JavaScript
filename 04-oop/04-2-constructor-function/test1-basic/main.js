36abf93e75744d69e4f210d6a7f5917b48386187
beofme31 <beome31@naver.com>
2024-09-24 13:19:16 +0900

    생성자 함수 정리

--- 04-oop/04-2-constructor-function/test1-basic/main.js
+++ 04-oop/04-2-constructor-function/test1-basic/main.js
@@ -39,5 +39,44 @@ function User2(name, age) {
 // 모형(생성자 함수)을 선언하고, 모형을 통해 동일 구조의 객체를 반복 생성
 let user1 = new User2('홍길동', 25);
 let user2 = new User2('김길동', 30);
-user1.sayHello();
-user2.sayHello();
+user1.sayHello(); // 홍길동, 25
+user2.sayHello(); // 김길동, 30
+
+// 생성자 함수와 리턴
+// 생성자 함수가 new로 호출이 되면 함수 내에서 명시적으로 return 시키지 않아도 생성된 객체가 리턴
+
+// 명시적으로 기초 데이터를 리턴 시킨 경우
+function User3(name, age) {
+  this.name = name;
+  this.age = age;
+  return 10;
+}
+let user3 = new User3('홍길동', 30);
+console.log(user3); // User3 {name: '홍길동', age: 30}
+// 기초 데이터 리턴시키면 무시되고 생성된 객체가 반환된다.
+
+// 명시적으로 코드에서 준비한 다른 객체를 리턴시킨다면
+function User4(name, age) {
+  this.name = name;
+  this.age = age;
+  return {
+    prodNo: 1,
+    prodName: '에어조던'
+  }
+}
+let user4 = new User4('홍길동', 30);
+console.log(user4); // {prodNo: 1, prodName: '에어조던'}
+// 객체를 리턴시키면 생성된 객체는 무시, 코드에서 리턴시킨 객체가 반환
+
+// 외부에서 객체 멤버 추가
+console.dir(user1); // User2
+console.dir(user2); // User2
+user1.address = 'seoul';
+user1.helloFun = function () {
+  console.log('hello');
+}
+// 추가한 객체에만 멤버가 존재
+// 동일한 생성자 함수로 생성된 객체라고 하더라도, 외부에서 멤버 추가하기 시작하면
+// 객체별 멤버가 다르게 된다.
+console.dir(user1);
+console.dir(user2);
