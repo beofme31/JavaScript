7a71ab1091ce28586d7b263304e4d7cf47b7a131
beofme31 <beome31@naver.com>
2024-09-29 18:30:21 +0900

    과제 조건 1 완료

--- 02-JAVASCRIPT-BASIC/02-7-event/test2-event-mouse/main.js
+++ 02-JAVASCRIPT-BASIC/02-7-event/test2-event-mouse/main.js
@@ -1,19 +1,19 @@
 "use strict";
 
 window.addEventListener('load', () => {
-  // click, dblclick, down, up
+  // click, dbclick, down, up
   let button = document.getElementById('button')
   button.addEventListener('click', () => {
-    console.log('mouse click')
+    console.log('mouse click');
   })
-  button.addEventListener('dblclick', () => {
-    console.log('mouse double click')
+  button.addEventListener('dbclick', () => {
+    console.log('mouse double click');
   })
   button.addEventListener('mousedown', () => {
-    console.log('mouse down')
+    console.log('mouse down');
   })
   button.addEventListener('mouseup', () => {
-    console.log('mouse up')
+    console.log('mouse up');
   })
 
   // mouse move
@@ -21,26 +21,10 @@ window.addEventListener('load', () => {
   let result = document.getElementById('result')
 
   area.addEventListener('mousemove', (e) => {
-    result.innerHTML = `offset(${e.offsetX},${e.offsetY}, page(${e.pageX}, ${e.pageY}))`
+    result.innerHTML = `offset(${e.offsetX}, ${e.offsetY}, page(${e.pageX}, ${e.pageY}))`
   })
 
-  // enter, over
-  let outer = document.getElementById('outer')
-  let inner = document.getElementById('inner')
 
-  outer.addEventListener('mouseenter', () => {
-    console.log('outer mouseenter')
-  })
 
-  inner.addEventListener('mouseenter', () => {
-    console.log('inner mouseenter')
-  })
-
-  outer.addEventListener('mouseover', () => {
-    console.log('outer mouseover')
-  })
-
-  inner.addEventListener('mouseover', () => {
-    console.log('inner mouseover')
-  })
 })
+
